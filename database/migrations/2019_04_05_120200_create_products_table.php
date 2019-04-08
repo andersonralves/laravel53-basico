46201a2ab373165044ac9a25442ceeefcfd9157b
Anderson <anderson.ti@plasutil.com.br>
2019-04-08 17:53:04 -0300

    Finalizada Aula 12

--- database/migrations/2019_04_05_120200_create_products_table.php
+++ database/migrations/2019_04_05_120200_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProdutosTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateProdutosTable extends Migration
      */
     public function up()
     {
-        Schema::create('produtos', function (Blueprint $table) {
-            $table->incrementes('id');
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
             $table->string('name', 150);
             $table->integer('number');
             $table->boolean('active');
-            $table->
+            $table->string('image');
+            $table->enum('category', ['eletronicos', 'moveis', 'limpeza', 'banho']);
+            $table->text('description');
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ class CreateProdutosTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('products');
     }
 }
